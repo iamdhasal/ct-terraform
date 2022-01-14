51dccb614d5aa324df9fbf4f38875a9c0e2c6abd
iamdhasal <80392774+iamdhasal@users.noreply.github.com>
2022-01-14 18:43:52 +0530

    Add files via upload

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+sudo apt-get update
 sudo apt-get install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
+echo "<html> <h1> Hello from Vivek from $HOSTNAME </h1> </html>">>/var/www/html/index.html
